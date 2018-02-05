5aea5823edd3266bc51175d5552f679454ac77c5
Tejasvi Royal <tejasvi.royal@gmail.com>
2018-02-05 17:59:35 +0530

    Add files via upload

---
+++ Prime_Or_Not_Given_By_PradeepGaru.java
@@ -0,0 +1,51 @@
+import java.io.*;
+
+public class Prime_Or_Not_Given_By_PradeepGaru
+{  	
+	static private boolean Prime_Or_Not_Given_By_PradeepGaru(int input)
+	{
+			int currentRemainder;
+			boolean divisibleFLAG = false,
+						  primeFLAG = false;
+			
+			if (input > 0 && input != 1)
+			{ 
+				for(int i = 2; i < input; i++)
+				{
+					currentRemainder = input % i;
+					divisibleFLAG = (currentRemainder == 0) ? true : false;
+					System.out.println("DIVISION TRACE: Divisible By " + i +": "+  divisibleFLAG);
+					
+					primeFLAG = (divisibleFLAG == false) ? true : false;
+				}
+				return primeFLAG;
+			}
+			else return false;
+	}
+	
+	static private boolean isPrimeNumber(int number)
+	{
+		if(number  == 2 || number ==3){
+			System.out.println("Prime Number!!!");
+		}else{
+			if(number % 2 == 0){
+			System.out.println("Not a prime Number");
+		}else if(number % 3 == 0 ){
+			System.out.println("Not a prime Number");
+		}else{
+			System.out.println("Prime Number!!!");
+		}
+		}
+		
+	}
+	
+	public static void main(String args[])
+	{
+		System.out.print("\nEnter the positive whole number: ");
+		//boolean output = Prime_Or_Not_Given_By_PradeepGaru(Integer.parseInt(System.console().readLine()));
+		//boolean output = isPrimeNumber(Integer.parseInt(System.console().readLine()));
+		//System.out.print("\nThe number is a Prime Number: " + output + "\n");
+
+ isPrimeNumber(Integer.parseInt(System.console().readLine()));		
+	}
+}
